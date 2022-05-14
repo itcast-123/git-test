dceff1a29696e8edf68e91a477205cf65e803f0f
xy <xy@163.com>
2022-05-14 13:25:22 +0800

    hot-fix test

--- src/main/java/p/GitTest.java
+++ src/main/java/p/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("123");
         System.out.println("hot-fix");
+        System.out.println("hot-fix");
     }
 
 }
