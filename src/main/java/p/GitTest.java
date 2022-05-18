01f91a4fbd22a62e14c6cfcae6a3860afcbb8bc2
itcast-123 <75197630+itcast-123@users.noreply.github.com>
2022-05-18 14:00:41 +0800

    remote test

--- src/main/java/p/GitTest.java
+++ src/main/java/p/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("123");
         System.out.println("third commit");
         System.out.println("remote change");
+        System.out.println("remote test");
     }
 
 }
